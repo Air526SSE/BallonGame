e78b3f18c538d280559274fbc3a8201c6372af3b
pierwzw <501311328@qq.com>
2014-10-31 20:18:30 +0800

    Merge remote-tracking branch 'origin/master'
    10311319

--- air526/src/com/su/MainScreenActivity.java
+++ air526/src/com/su/MainScreenActivity.java
@@ -1,15 +1,16 @@
 package com.su;
 
-import com.tu.Balloon;
-import com.tu.MusicServer;
-
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.tu.Balloon;
+import com.tu.MusicServer;
 
 public class MainScreenActivity extends Activity {
 
@@ -22,14 +23,13 @@ public class MainScreenActivity extends Activity {
 		setContentView(R.layout.main_screen);
 		
 		
-	//	startService(new Intent(MainScreenActivity.this, MusicServer.class));
+		//startService(new Intent(MainScreenActivity.this, MusicServer.class));
 	
 		start = (Button)findViewById(R.id.button3);
 		cancle = (Button)findViewById(R.id.button2);
 		about = (Button)findViewById(R.id.button1);
 		
-		start.setOnClickListener(new View.OnClickListener() {
-			
+		start.setOnClickListener(new View.OnClickListener() {			
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
@@ -46,7 +46,26 @@ public class MainScreenActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				MainScreenActivity.this.finish();
+				/*finish();
+				System.exit(0);
+				android.os.Process.killProcess(android.os.Process.myPid()); 
+				ActivityManager activityMgr= (ActivityManager) getSystemService(ACTIVITY_SERVICE );  
+	            activityMgr.killBackgroundProcesses(getPackageName());*/
+	            
+	            /*int currentVersion = android.os.Build.VERSION.SDK_INT;
+	            if (currentVersion > android.os.Build.VERSION_CODES.ECLAIR_MR1) {
+	            Intent startMain = new Intent(Intent.ACTION_MAIN);
+	            startMain.addCategory(Intent.CATEGORY_HOME);
+	            startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+	            startActivity(startMain);
+	            System.exit(0);
+	            } else {// android2.1
+	            ActivityManager am = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+	            am.restartPackage(getPackageName());
+	            	System.exit(0);
+	            
+	            }*/
+				System.exit(0);
 			}
 		});
 		
